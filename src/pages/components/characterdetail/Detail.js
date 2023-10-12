183907b933cb739e39b770cd4397903fc3af178a
unknown <digyrh456789@gmail.com>
2023-10-12 17:50:51 +0900

    fix: css 오류

--- src/pages/components/characterdetail/Detail.js
+++ src/pages/components/characterdetail/Detail.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { useRouter } from 'next/router';
 import axios from "axios";
 import Headers from "@/app/Header";
-import styles from '@/styles/components/characterdetail/Detail.module.css';
+import styles from '../../../styles/components/characterdetail/Detail.module.css';
 export default function Detail(props) {
     const [elementals, setElementals] = useState();
     const [chid, setChid] = useState();
