e9bd99085e44420152fabcaccba7a018ad7f9f07
unknown <digyrh456789@gmail.com>
2023-10-13 17:53:30 +0900

    chore:

---
+++ config/config
@@ -0,0 +1,4 @@
+export const prefix =
+  process.env.NODE_ENV === "production"
+    ? "https://EngChooN.github.io/joonyoung-s-portfolio-page"
+    : "";
--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const debug = process.env.NODE_ENV !== "production";
+const repository = "https://genshin-impact-info.github.io/GenshinImpact_inform";
+const nextConfig = {
+    output: 'export',
+    reactStrictMode: true,
+    assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
+    trailingSlash: true,
+}
+
+module.exports = nextConfig;
