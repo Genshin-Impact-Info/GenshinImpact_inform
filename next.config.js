2f2b8f66f4ec7d8e5775210140a40c1bf9e27e6c
unknown <digyrh456789@gmail.com>
2023-10-16 10:10:58 +0900

    chore:

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const debug = process.env.NODE_ENV !== "production";
-const repository = "https://genshin-impact-info.github.io/GenshinImpact_inform";
 const nextConfig = {
-    output: 'export',
-    reactStrictMode: true,
-    assetPrefix: !debug ? `/${repository}/` : "", // production 일때 prefix 경로
-    trailingSlash: true,
+    basePath: "/"
 }
 
 module.exports = nextConfig;
